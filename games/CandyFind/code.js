e9b071c893fc31105ad61a02fe5a0c053055c676
Adi2p30 <55141249+Adi2p30@users.noreply.github.com>
2024-04-05 12:31:59 +0530

    Make the Code Prettier

--- games/CandyFind/code.js
+++ games/CandyFind/code.js
@@ -1,38 +1,44 @@
 var Score;
 var Score = 0;
-onEvent("INSTRUCTIONSBU", "click", function(event) {
+onEvent("INSTRUCTIONSBU", "click", function (event) {
   console.log("button1 clicked!");
   setScreen("Worlds");
 });
-onEvent("PLAYBU", "click", function(event) {
+onEvent("PLAYBU", "click", function (event) {
   console.log("button2 clicked!");
   setScreen("Insructions");
 });
-onEvent("THECANDYIM", "click", function(event) {
+onEvent("THECANDYIM", "click", function (event) {
   console.log("THECANDYIM clicked!");
   Score = Score + 1;
   setText("SCORELB", Score);
   if (Score >= 10) {
     setScreen("WinScreen");
   }
-  setPosition("THECANDYIM", randomNumber(0, 300), randomNumber(1, 350), 41, 110);
+  setPosition(
+    "THECANDYIM",
+    randomNumber(0, 300),
+    randomNumber(1, 350),
+    41,
+    110
+  );
 });
 
-onEvent("CLUSTERBG", "click", function(event) {
+onEvent("CLUSTERBG", "click", function (event) {
   console.log("CLUSTERBG clicked!");
   setScreen("LoseScreen");
 });
-onEvent("button2", "click", function(event) {
+onEvent("button2", "click", function (event) {
   console.log("button2 clicked!");
   setScreen("Insructions");
   Score = 0;
 });
-onEvent("LoseScreen", "click", function(event) {
+onEvent("LoseScreen", "click", function (event) {
   console.log("LoseScreen clicked!");
   setScreen("Insructions");
   Score = 0;
 });
-onEvent("button1", "click", function(event) {
+onEvent("button1", "click", function (event) {
   console.log("button1 clicked!");
   setScreen("GameScreen");
 });
--- games/Color Sleuth FP/code.js
+++ games/Color Sleuth FP/code.js
@@ -9,28 +9,32 @@ function Set_Board() {
   Red = randomNumber(1, 255);
   green = randomNumber(1, 255);
   blue = randomNumber(1, 255);
-  setProperty("button1", "background-color", rgb(Red,green,blue));
-  setProperty("button2", "background-color", rgb(Red,green,blue));
-  setProperty("button3", "background-color", rgb(Red,green,blue));
-  setProperty("button4", "background-color", rgb(Red,green,blue));
+  setProperty("button1", "background-color", rgb(Red, green, blue));
+  setProperty("button2", "background-color", rgb(Red, green, blue));
+  setProperty("button3", "background-color", rgb(Red, green, blue));
+  setProperty("button4", "background-color", rgb(Red, green, blue));
   oddone = randomNumber(1, 4);
-  setProperty("button" + oddone, "background-color", rgb(Red + 25, green + 25, blue + 25));
+  setProperty(
+    "button" + oddone,
+    "background-color",
+    rgb(Red + 25, green + 25, blue + 25)
+  );
 }
 function Checkanswer(Buttoncheck) {
   if (Buttoncheck == "button" + oddone) {
     if (CurrPlay == 1) {
-      Player1 ++;
+      Player1++;
       setText("score1_label", Player1);
     } else {
-      Player2 ++;
+      Player2++;
       setText("score2_label", Player2);
     }
   } else {
-    if (CurrPlay==1) {
-      Player1--
+    if (CurrPlay == 1) {
+      Player1--;
       setText("score1_label", Player1);
     } else {
-      Player2--
+      Player2--;
       setText("score2_label", Player2);
     }
   }
@@ -47,22 +51,22 @@ function Switchplayer() {
     setProperty("player2_highlight", "hidden", true);
   }
 }
-onEvent("button1", "click", function( ) {
+onEvent("button1", "click", function () {
   Checkanswer("button1");
-Switchplayer();
+  Switchplayer();
   Set_Board();
 });
-onEvent("button2", "click", function( ) {
+onEvent("button2", "click", function () {
   Checkanswer("button2");
   Switchplayer();
   Set_Board();
 });
-onEvent("button3", "click", function( ) {
+onEvent("button3", "click", function () {
   Checkanswer("button3");
   Switchplayer();
   Set_Board();
 });
-onEvent("button4", "click", function( ) {
+onEvent("button4", "click", function () {
   Checkanswer("button4");
   Switchplayer();
   Set_Board();
@@ -71,33 +75,37 @@ function Set_Board2() {
   Red = randomNumber(1, 255);
   green = randomNumber(1, 255);
   blue = randomNumber(1, 255);
-  setProperty("button100", "background-color", rgb(Red,green,blue));
-  setProperty("button101", "background-color", rgb(Red,green,blue));
-  setProperty("button102", "background-color", rgb(Red,green,blue));
-  setProperty("button103", "background-color", rgb(Red,green,blue));
-  setProperty("button104", "background-color", rgb(Red,green,blue));
-  setProperty("button105", "background-color", rgb(Red,green,blue));
-  setProperty("button106", "background-color", rgb(Red,green,blue));
-  setProperty("button107", "background-color", rgb(Red,green,blue));
-  setProperty("button108", "background-color", rgb(Red,green,blue));
+  setProperty("button100", "background-color", rgb(Red, green, blue));
+  setProperty("button101", "background-color", rgb(Red, green, blue));
+  setProperty("button102", "background-color", rgb(Red, green, blue));
+  setProperty("button103", "background-color", rgb(Red, green, blue));
+  setProperty("button104", "background-color", rgb(Red, green, blue));
+  setProperty("button105", "background-color", rgb(Red, green, blue));
+  setProperty("button106", "background-color", rgb(Red, green, blue));
+  setProperty("button107", "background-color", rgb(Red, green, blue));
+  setProperty("button108", "background-color", rgb(Red, green, blue));
   oddone = randomNumber(100, 108);
-  setProperty("button" + oddone, "background-color", rgb(Red + 25, green + 25, blue + 25));
+  setProperty(
+    "button" + oddone,
+    "background-color",
+    rgb(Red + 25, green + 25, blue + 25)
+  );
 }
 function Checkanswer2(Buttoncheck) {
   if (Buttoncheck == "button" + oddone) {
     if (CurrPlay == 1) {
-      Player1 ++;
+      Player1++;
       setText("label6", Player1);
     } else {
-      Player2 ++;
+      Player2++;
       setText("label9", Player2);
     }
   } else {
-    if (CurrPlay==1) {
-      Player1--
+    if (CurrPlay == 1) {
+      Player1--;
       setText("label6", Player1);
     } else {
-      Player2--
+      Player2--;
       setText("label9", Player2);
     }
   }
@@ -114,61 +122,61 @@ function Switchplayer2() {
     setProperty("secondlight", "hidden", true);
   }
 }
-onEvent("button100", "click", function( ) {
+onEvent("button100", "click", function () {
   Checkanswer2("button100");
-Switchplayer2();
+  Switchplayer2();
   Set_Board2();
 });
-onEvent("button101", "click", function( ) {
+onEvent("button101", "click", function () {
   Checkanswer2("button101");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button102", "click", function( ) {
+onEvent("button102", "click", function () {
   Checkanswer2("button102");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button103", "click", function( ) {
+onEvent("button103", "click", function () {
   Checkanswer2("button103");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button104", "click", function( ) {
+onEvent("button104", "click", function () {
   Checkanswer2("button104");
-Switchplayer2();
+  Switchplayer2();
   Set_Board2();
 });
-onEvent("button105", "click", function( ) {
+onEvent("button105", "click", function () {
   Checkanswer2("button105");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button106", "click", function( ) {
+onEvent("button106", "click", function () {
   Checkanswer2("button106");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button107", "click", function( ) {
+onEvent("button107", "click", function () {
   Checkanswer2("button107");
   Switchplayer2();
   Set_Board2();
 });
-onEvent("button108", "click", function( ) {
+onEvent("button108", "click", function () {
   Checkanswer2("button108");
   Switchplayer2();
   Set_Board2();
 });
 setScreen("screen1");
-onEvent("button5", "click", function(event) {
+onEvent("button5", "click", function (event) {
   console.log("button5 clicked!");
   setScreen("screen2");
 });
-onEvent("button7", "click", function(event) {
+onEvent("button7", "click", function (event) {
   console.log("button5 clicked!");
   setScreen("gamePlay_screen");
 });
-onEvent("button8", "click", function(event) {
+onEvent("button8", "click", function (event) {
   console.log("button5 clicked!");
   Set_Board2();
   setScreen("screen3");
