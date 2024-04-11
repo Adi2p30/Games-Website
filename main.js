032fe8425ddc7a0755c9ae1cd26781c4b0db6879
Adi2p30 <55141249+Adi2p30@users.noreply.github.com>
2024-04-11 16:16:41 +0530

    Added Touchscreen to Helicopter

---
+++ main.js
@@ -0,0 +1,3 @@
+$('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
+
+$('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
